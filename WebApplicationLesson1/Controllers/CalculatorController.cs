cdcfe1c5278f2db6d2454d237d2d84dd61d41d97
Saba Bestaev <bestaev.saba@gmail.com>
2024-04-02 15:43:11 +0300

    Добавил оперцию деления

---
+++ WebApplicationLesson1/Controllers/CalculatorController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationLesson1.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public string Index(double a, double b, char operation)
+        {
+            switch (operation)
+            {
+                case '-':
+                    return $"{a} {operation} {b} = {a - b}";
+                case '*':
+                    return $"{a} {operation} {b} = {a * b}";
+                case '/':
+                    return $"{a} {operation} {b} = {a / b}";
+                default:
+                    if (operation != '+' && operation != 0) return "Вы можете применить только операции сложения, вычитания и умножения";
+                    return $"{a} + {b} = {a + b}";
+            }
+        }
+    }
+}
