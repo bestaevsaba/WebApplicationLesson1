55a7f255f9a051300f04abca44075e934726c543
Saba Bestaev <bestaev.saba@gmail.com>
2024-04-02 15:24:43 +0300

    Добавил новый контроллер

--- WebApplicationLesson1/Startup.cs
+++ WebApplicationLesson1/Startup.cs
@@ -3,10 +3,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace WebApplicationLesson1
 {
@@ -45,8 +41,8 @@ namespace WebApplicationLesson1
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{a?}/{b?}");
             });
         }
     }
